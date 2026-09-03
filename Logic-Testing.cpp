86090b499ff88376b57b6757af834ababf7ada3d
gwabaaa <galangmuda1507@gmail.com>
2026-09-03 10:35:00 +0700

    debug

--- Logic-Testing.cpp
+++ Logic-Testing.cpp
@@ -3,7 +3,47 @@ using namespace std;
 char buffer[7] = {'A', 'N', 'D', ' ', '1', ' ', '0'};
 int oprand1, oprand2, varoprator;
 
-int sv = 0;
+char calculatespecial(int d1, int d2, int op) {
+    if (op == 0) {
+        if (d1 == 1 && d2 == 1) {
+            return '0';
+        } else {
+            return '1';
+        }
+    } else if (op == 1) {
+        if ((d1 == 1 && d2 == 1) || (d1 == 0 && d2 == 0)) {
+            return '1';
+        } else {
+            return '0';
+        }
+    } else if (op == 2) {
+        if (d1 == '1' && d2 == '1') {
+            return '1' ;
+        } else {
+            return '0';
+        }
+    } else if (op == 3) {
+        if ((d1 == 1 && d2 == 0) || (d1 == 0 && d2 == 1)) {
+            return '1';
+        } else {
+            return '0';
+        }
+    } else if (op == 4) {
+        if (d1 == 0 && d2 == 0) {
+            return '1';
+        } else {
+            return '0';
+        }
+    } else if (op == 5) {
+        if (d1 == 0) {
+            return '1';
+        } else {
+            return '0';
+        }
+    } else {
+        return 'x';
+    }
+}
 
 // nand xnor
 void token4digit(){
@@ -11,38 +51,46 @@ void token4digit(){
     oprand2 = buffer[7];
     if (buffer[0] == 'N' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'D') {
         varoprator = 0;
-        calculatespecial(oprand1, oprand2, varoprator);
     } else if (buffer[0] == 'X' && buffer[1] == 'N' && buffer[2] == 'O' && buffer[3] == 'R') {
         varoprator = 1;
-        calculatespecial(oprand1, oprand2, varoprator);
     } else {
-        cout << "invalid" << endl;
+        cout << "x" << endl;
     }
+    cout<<calculatespecial(oprand1, oprand2, varoprator)<<endl;
 }
 
 // and xor nor not
 void token3digit(){
     oprand1 = buffer[4];
     oprand2 = buffer[6];
+    cout<<"stored digits"<<endl;
+    for (int i = 0; i < 3; i++) {
+        cout<<buffer[i]<<endl;
+    }
     if (buffer[1] == 'O') {
+        cout<<"o found" << endl;
         if (buffer[2] == 'T' && buffer[0] == 'N') {
+            cout<<"mode 3 digit not" << endl;
             varoprator = 5;
-            calculatespecial(oprand1, oprand2, varoprator);
         } else if (buffer[2] == 'R') {
         if (buffer[0] == 'X') {
+            cout<<"mode 3 digit xor" << endl;
             varoprator = 3;
-            calculatespecial(oprand1, oprand2, varoprator);
         } else if (buffer[0] == 'N') {
+            cout<<"mode 3 digit nor" << endl;
             varoprator = 4;
-            calculatespecial(oprand1, oprand2, varoprator);
         }
     } else if (buffer[0] == 'A' && buffer[1] == 'N' && buffer[2] == 'D') {
+        cout<<"mode 3 digit and" << endl;
         varoprator = 2;
-        calculatespecial(oprand1, oprand2, varoprator);
     } else {
-        cout << "invalid" << endl;
+        cout << "x" << endl;
     }
     }
+    cout<<oprand1<<endl;
+    cout<<oprand2<<endl;
+    cout<<varoprator<<endl;
+    cout<<calculatespecial(oprand1, oprand2, varoprator)<<endl;
 }
 
 // + - * /
@@ -56,49 +104,73 @@ void token1digit(){
     } else if (buffer[2] == '/') {
         cout << oprand1 / oprand2 << endl;
     } else {
-        cout << "invalid" << endl;
+        cout << "x" << endl;
     }
 }
 
-int calculatespecial(int d1, int d2, int op) {
-    if (op == 1) {
-        return d1 + d2;
-    } else if (op == 2) {
-        return d1 - d2;
-    } else if (op == 3) {
-        return d1 * d2;
-    } else if (op == 4) {
-        return d1 / d2;
-    } else {
-        cout << "invalid" << endl;
-        return -1;
+bool checknumber(int index1, int index2) {
+    if (buffer[index1] >= '0' && buffer[index1] <= '9') {
+        if (buffer[index2] >= '0' && buffer[index2] <= '9') {
+            return true;
+        }
     }
+    return false;
+}
 
+bool checkdigit(int index1, int index2) {
+    if (buffer[index1] == '0' || buffer[index1] == '1') {
+        if (buffer[index2] == '0' || buffer[index2] == '1') {
+            return true;
+        }
+    }
+    return false;
 }
 
 int main() {
 
-for (int i = 0; i < 7; i++) {
+for (int i = 0; i < 5; i++) {
     if (buffer[i] != ' ') {
         continue;
     } else {
-        cout << "detected space" << endl;
         if (i == 1) {
-            oprand1 = buffer[0];
-            oprand2 = buffer[4];
-            token1digit();           
+            if (checknumber(0, 4) == true) {
+                oprand1 = buffer[0] - '0';
+                oprand2 = buffer[4] - '0';
+                token1digit();
+            } else {
+                cout << "x" << endl;
+            }       
         } else if (i == 2) { //or
-            cout << buffer[3] || buffer[5] << endl;
+            if (checkdigit(3, 5) == true) {
+                oprand1 = buffer[3];
+                oprand2 = buffer[5];
+            } else {
+                cout << "x" << endl;
+            }
+            if (oprand1 == '0' && oprand2 == '0') {
+                cout << "0" << endl;
+            } else {
+                cout << "1" << endl;
+            }
         } else if (i == 3) {
-            token3digit();
+            if (checkdigit(4, 6) == true) {
+                cout << "mode 3 digit" << endl;
+                token3digit();
+            } else {
+                cout << "x" << endl;
+            }
         } else if (i == 4) {
-            token4digit();
+            if (checkdigit(5, 7) == true) {
+                cout << "mode 4 digit" << endl;
+                token4digit();
+            } else {
+                cout << "x" << endl;
+            }
         } else {
-            cout << "invalid" << endl;
-            break;
+            cout << "x" << endl;
         }
+        break;
     }
 }
-
 return 0;
 }
